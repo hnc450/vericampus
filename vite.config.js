8ba0b9b6f279b5c70bbe95236aeebdf15cc66f99
henock <henoctumonakiese@gmail.com>
2026-09-01 11:22:59 +0100

    fix(deploy): Update basename

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vericampus/', 
+  base: '/docs/', 
   plugins: [react(), tailwindcss()],
 })
